2bcde677756e0b90f10ed3acb4b13dbbe403fd83
ShotaTanaka <>
2018-10-15 18:35:01 +0900

    7-6

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -16,6 +16,21 @@ public class PTra07_06 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
+		int sum = 0;
+		int count = 0;
+		for (int i = 0;i < array.length;i++) {
+			
+			for (int j = 0; j < array[i].length; j++) {
+				sum += array[i][j];
+				count++;
+			}
+		}
+		System.out.println(sum/count);
+		
+		
+//		double sum = array[0][0]+array[0][1]+array[0][2]+array[1][0]+array[1][1]+array[1][2];
+//		
+//		System.out.println(sum / array[0].length+array[1].length);
 
 	}
 }
