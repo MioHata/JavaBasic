8007e7616d91e4cd17f5140ce3b2bd1b2325b00b
ShotaTanaka <>
2018-10-16 18:29:51 +0900

    10-4

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -26,11 +26,18 @@ public class PTra10_01 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
+		User us;
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
+		us = new User();
+//		us.userId = 121212;
+//		us.userNm = "ららら";
+//		us.mail = "au@ezweb.ne.jp";
+//		us.password = "hfejfj0819";
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		System.out.println(us.userId);
+		System.out.println(us.userNm);
+		System.out.println(us.mail);
+		System.out.println(us.password);
 	}
 }
