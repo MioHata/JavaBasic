84c7b5ccb9185f9b9e2349418b663b2e44bb5762
ShotaTanaka <>
2018-10-18 15:29:15 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,18 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[] bookList = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String s = new java.util.Scanner(System.in).nextLine();
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+		for (int i = 0; i <bookList.length;i++) {
+			if (bookList[i].title.contains(s)) {
+				System.out.println(bookList[i].dispBookInfo());
+				
+			}
+		}
 	}
 }
+
