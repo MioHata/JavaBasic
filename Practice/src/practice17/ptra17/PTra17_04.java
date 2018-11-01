3826caf7011608e556437da877f1369c04543589
ShotaTanaka <>
2018-11-01 15:10:58 +0900

    17-4

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -16,5 +16,20 @@ public class PTra17_04 {
 		 * ※ Thread.sleep を使用すること
 		 */
 
+		dispMessageSlowly(str,1000);
+
+	}
+
+	public static void dispMessageSlowly(String message, long millisecond) {
+		char[] array = message.toCharArray();
+		for (int i = 0; i < message.length(); i++) {
+			System.out.print(array[i]);
+			try {
+				Thread.sleep(millisecond);
+			} catch (InterruptedException e) {
+				/* エラーが発生した場合はここにくる */
+			}
+		}
 	}
 }
+
